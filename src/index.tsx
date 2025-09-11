78bb641151a2cab513529a64a114cbec444a8bee
skumar <kumasachin@gmail.com>
2025-09-11 17:49:07 +0100

    Init Piral PLM setup

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+// Entry point for the Piral microfrontend boilerplate
+import React from "react";
+// ...existing code...
+
+const App = () => {
+  return <div>PLM Piral Microfrontend Boilerplate</div>;
+};
+
+export default App;
